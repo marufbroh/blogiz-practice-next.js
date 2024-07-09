3394bf764367b1b2f77bcc973ba47c306ca123d9
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-07-09 12:39:07 +0600

    Show Off All The Fantastic Blogs Using SSR

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,30 @@
+import BlogCard from "@/components/ui/BlogCard";
+import React from "react";
+
+const BlogsPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    cache: "no-store"
+  });
+  const blogs = await res.json();
+  return (
+    <div className="w-[90%] mx-auto my-5">
+      <h1 className="text-4xl text-center my-5">
+        All blogs from <span className="text-accent">Blogiz</span>
+      </h1>
+      <p className="text-xl text-center text-gray-400 w-2/4 mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+
+      <div className="grid grid-cols-4 gap-4 my-5">
+        {blogs.map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
